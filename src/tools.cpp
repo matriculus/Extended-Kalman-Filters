7ca2347c4e013f3eba5242e2de39dc66446b6550
Vishnu Pradeesh <pradeeshvishnu@outlool.com>
2019-02-17 00:00:43 +0530

    Update tools.cpp

--- src/tools.cpp
+++ src/tools.cpp
@@ -2,7 +2,6 @@
 #include "tools.h"
 
 using Eigen::VectorXd;
-using Eigen::MatrixXd;
 using std::vector;
 
 Tools::Tools() {}
@@ -28,31 +27,4 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
  RMSE /= estimations.size();
  RMSE = RMSE.array().sqrt();
  return RMSE;
-}
-
-MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-  TODO:
-    * Calculate a Jacobian here.
-  */
-  MatrixXd Hj(3, 4);
-  // state variables
-  float px = x_state(0);
-  float py = x_state(1);
-  float vx = x_state(2);
-  float vy = x_state(3);
-
-  float c1 = px*px + py*py;
-  float c2 = sqrt(c1);
-  float c3 = (c1*c2);
-
-  if (fabs(c1) < 0.0001){
-    std::cout << "Division by Zero!" << std::endl;
-    return Hj;
-  }
-  Hj << (px/c2), (py/c2), 0, 0,
-        -(py/c1), (px/c1), 0, 0,
-        py*(vx*py - vy*px) / c3, px*(px*vy - py*vx) / c3, px / c2, py / c2;
-  
-  return Hj;
 }
